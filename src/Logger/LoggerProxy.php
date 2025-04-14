91c4a411f4794bf853db976a03f16d8cf8fcdb43
Daniel Dahme <dahme@qualitus.de>
2025-04-14 17:14:19 +0200

    microsevrice proxy with connection check

--- src/Logger/LoggerProxy.php
+++ src/Logger/LoggerProxy.php
@@ -2,9 +2,10 @@
 
 namespace Base3\Logger;
 
+use Base3\Api\ICheck;
 use Base3\Logger\Api\ILogger;
 
-class LoggerProxy implements ILogger {
+class LoggerProxy implements ILogger, ICheck {
 
 	private $connector;
 
@@ -26,5 +27,11 @@ class LoggerProxy implements ILogger {
 
 	public function getLogs(string $scope, int $num = 50, bool $reverse = true): array {
 		return $this->connector->getLogs($scope, $num, $reverse);
+	}
+
+	// Implementation of ICheck
+
+	public function checkDependencies() {
+		return $this->connector instanceof ICheck ? $this->connector->checkDependencies() : [];
 	}
 }
