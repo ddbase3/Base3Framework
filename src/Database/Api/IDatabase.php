91e14068763b91f363e732cf84ae7a9cd31351ac
Daniel Dahme <info@base3.de>
2025-05-13 23:37:48 +0200

    docs for interface

--- src/Database/Api/IDatabase.php
+++ src/Database/Api/IDatabase.php
@@ -8,11 +8,31 @@ interface IDatabase {
 	public function connected();
 	public function disconnect();
 
+	/**
+	 * inserts, updates, deletes without response
+	 */
 	public function nonQuery($query);
+
+	/**
+	 * response is just a single value
+	 */
 	public function scalarQuery($query);
+
+	/**
+	 * response is just a single row
+	 */
 	public function singleQuery($query);
+
+	/**
+	 * response is a list of value (a column)
+	 */
 	public function &listQuery($query);
+
+	/**
+	 * response is a list of rows
+	 */
 	public function &multiQuery($query);
+
 	public function affectedRows();
 	public function insertId();
 	public function escape($str);
