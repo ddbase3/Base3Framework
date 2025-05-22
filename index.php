fae7683e627f1c7872f9023d2d5ac962ffae1e3f
Daniel Dahme <info@base3.de>
2025-05-22 03:08:40 +0200

    bootstrap

--- index.php
+++ index.php
@@ -1,65 +1,10 @@
-<?php
+<?php declare(strict_types=1);
 
-/* Debug mode - 0: aus, 1: an, ggfs noch höhere Stufen? */
-putenv('DEBUG=1');
+require __DIR__ . '/src/Core/Autoloader.php';
+\Base3\Core\Autoloader::register();
 
-/* error handling */
-ini_set('display_errors', getenv('DEBUG') ? 1 : 0);
-ini_set('display_startup_errors', getenv('DEBUG') ? 1 : 0);
-error_reporting(getenv('DEBUG') ? E_ALL | E_STRICT : 0);
+$composerAutoload = __DIR__ . '/vendor/autoload.php';
+if (file_exists($composerAutoload)) require_once $composerAutoload;
 
-/* define directories constants */
-define('DIR_ROOT', __DIR__ . DIRECTORY_SEPARATOR);
-define('DIR_CNF', DIR_ROOT . 'cnf' . DIRECTORY_SEPARATOR);
-define('DIR_SRC', DIR_ROOT . 'src' . DIRECTORY_SEPARATOR);
-define('DIR_LOCAL', DIR_ROOT . 'local' . DIRECTORY_SEPARATOR);
-define('DIR_PLUGIN', DIR_ROOT . 'plugin' . DIRECTORY_SEPARATOR);
-define('DIR_TEST', DIR_ROOT . 'test' . DIRECTORY_SEPARATOR);
-define('DIR_TMP', DIR_ROOT . 'tmp' . DIRECTORY_SEPARATOR);
-define('DIR_TPL', DIR_ROOT . 'tpl' . DIRECTORY_SEPARATOR);
-define('DIR_USERFILES', DIR_ROOT . 'userfiles' . DIRECTORY_SEPARATOR);
-
-/* uses */
-use Base3\Api\IClassMap;
-use Base3\Api\IContainer;
-use Base3\Api\IPlugin;
-use Base3\Api\IRequest;
-use Base3\Configuration\Api\IConfiguration;
-use Base3\Configuration\ConfigFile\ConfigFile;
-use Base3\Core\Autoloader;
-use Base3\Core\PluginClassMap;
-use Base3\Core\Request;
-use Base3\Core\ServiceLocator;
-use Base3\ServiceSelector\Api\IServiceSelector;
-use Base3\ServiceSelector\Standard\StandardServiceSelector;
-
-/* autoloader */
-require DIR_SRC . 'Core/Autoloader.php';
-Autoloader::register();
-
-/* autoloader: Composer (optional) */
-$pluginComposerAutoload = DIR_ROOT . 'vendor/autoload.php';
-if (file_exists($pluginComposerAutoload)) require_once $pluginComposerAutoload;
-
-/* service locator */
-$servicelocator = new ServiceLocator();
-ServiceLocator::useInstance($servicelocator);
-$servicelocator
-	->set('servicelocator', $servicelocator, ServiceLocator::SHARED)
-	->set(IRequest::class, Request::fromGlobals(), ServiceLocator::SHARED)
-	->set(IContainer::class, 'servicelocator', ServiceLocator::ALIAS)
-	->set('configuration', new ConfigFile, ServiceLocator::SHARED)
-	->set(IConfiguration::class, 'configuration', ServiceLocator::ALIAS)
-	->set('classmap', new PluginClassMap($servicelocator), ServiceLocator::SHARED)
-	->set(IClassMap::class, 'classmap', ServiceLocator::ALIAS)
-	->set(IServiceSelector::class, StandardServiceSelector::getInstance(), ServiceLocator::SHARED)
-	;
-
-/* plugins */
-$plugins = $servicelocator->get(IClassMap::class)->getInstancesByInterface(IPlugin::class);
-foreach ($plugins as $plugin) $plugin->init();
-
-/* go */
-$serviceselector = $servicelocator->get(IServiceSelector::class);
-$serviceselector->go();
-
+require __DIR__ . '/src/Core/Bootstrap.php';
+(new \Base3\Core\Bootstrap())->run();
