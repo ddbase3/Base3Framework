28316fe96b8a3b71ed208bc30c20698b91e46e98
Daniel Dahme <info@base3.de>
2025-05-29 15:14:02 +0200

    getName static

--- src/Core/AbstractOutput.php
+++ src/Core/AbstractOutput.php
@@ -9,19 +9,19 @@ abstract class AbstractOutput implements IOutput {
 	// Implementation of IBase
 
 	public static function getName(): string {
-		return strtolower($this->getClassName());
+		return strtolower(static::getClassName());
 	}
 
 	// Implementation of IOutput
 
         public function getHelp(): string {
-                return 'Help of ' . $this->getClassName() . "\n";
+                return 'Help of ' . static::getClassName() . "\n";
         }
 
 	// Private methods
 
-	private function getClassName(): string {
-		return (new \ReflectionClass($this))->getShortName();
+	private static function getClassName(): string {
+		return basename(str_replace('\\', '/', static::class));
 	}
 }
 
