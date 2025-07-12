1529babac33950f4f41a060096c544e0ed3bddb3
Daniel Dahme <info@base3.de>
2025-07-12 22:57:29 +0200

    bootstrap fix

--- src/Core/Bootstrap.php
+++ src/Core/Bootstrap.php
@@ -7,6 +7,8 @@ use Base3\Api\IClassMap;
 use Base3\Api\IContainer;
 use Base3\Api\IPlugin;
 use Base3\Api\IRequest;
+use Base3\Accesscontrol\Api\IAccesscontrol;
+use Base3\Accesscontrol\No\NoAccesscontrol;
 use Base3\Core\Request;
 use Base3\Core\ServiceLocator;
 use Base3\Configuration\ConfigFile\ConfigFile;
@@ -15,8 +17,8 @@ use Base3\Core\PluginClassMap;
 use Base3\Hook\IHookManager;
 use Base3\Hook\IHookListener;
 use Base3\Hook\HookManager;
-use Base3\ServiceSelector\Standard\StandardServiceSelector;
 use Base3\ServiceSelector\Api\IServiceSelector;
+use Base3\ServiceSelector\Standard\StandardServiceSelector;
 
 class Bootstrap implements IBootstrap {
 
@@ -29,12 +31,15 @@ class Bootstrap implements IBootstrap {
 			->set('servicelocator', $container, IContainer::SHARED)
 			->set(IRequest::class, fn() => Request::fromGlobals(), IContainer::SHARED)
 			->set(IContainer::class, 'servicelocator', IContainer::ALIAS)
-			->set(IHookManager::class, fn() => new HookManager, IContainer::SHARED)
-			->set('configuration', fn() => new ConfigFile, IContainer::SHARED)
+			->set(IHookManager::class, fn() => new HookManager(), IContainer::SHARED)
+			->set('configuration', fn() => new ConfigFile(), IContainer::SHARED)
 			->set(IConfiguration::class, 'configuration', IContainer::ALIAS)
 			->set('classmap', fn($c) => new PluginClassMap($c->get(IContainer::class)), IContainer::SHARED)
 			->set(IClassMap::class, 'classmap', IContainer::ALIAS)
-			->set(IServiceSelector::class, fn() => StandardServiceSelector, IContainer::SHARED)
+                        ->set('accesscontrol', fn($c) => new NoAccesscontrol(), IContainer::SHARED)
+                        ->set(IAccesscontrol::class, 'accesscontrol', IContainer::ALIAS)
+			->set(IServiceSelector::class, fn() => new StandardServiceSelector(), IContainer::SHARED)
+			->set('middlewares', [])
 		;
 
 		// hooks
