beef23897f2b03ab952d1adbd252117547c59b74
Daniel Dahme <info@base3.de>
2026-06-13 23:39:39 +0200

    refactoring delegate worker microservice

--- src/Worker/DelegateWorkerMicroservice.php
+++ src/Worker/DelegateWorkerMicroservice.php
@@ -1,7 +1,25 @@
 <?php declare(strict_types=1);
 
+/***********************************************************************
+ * This file is part of BASE3 Framework.
+ *
+ * BASE3 Framework is a lightweight, modular PHP framework for scalable
+ * and maintainable web applications. Built for extensibility,
+ * performance, and modern development, it can run standalone or
+ * integrate as a subsystem within a host system.
+ *
+ * Developed by Daniel Dahme
+ * Licensed under GPL-3.0
+ * https://www.gnu.org/licenses/gpl-3.0.en.html
+ *
+ * https://base3.de
+ * https://github.com/ddbase3/Base3Framework
+ **********************************************************************/
+
 namespace Base3\Worker;
 
+use Base3\Api\IClassMap;
+use Base3\Configuration\Api\IConfiguration;
 use Base3\Worker\DelegateWorker;
 use Base3\Worker\Api\IWorker;
 use Base3\Microservice\AbstractMicroservice;
@@ -10,8 +28,11 @@ class DelegateWorkerMicroservice extends AbstractMicroservice implements IWorker
 
 	private $worker;
 
-	public function __construct() {
-		$this->worker = new DelegateWorker;
+	public function __construct(
+		private readonly IClassMap $classmap,
+		private readonly IConfiguration $configuration
+	) {
+		$this->worker = new DelegateWorker($this->classmap, $this->configuration);
 	}
 
 	// Implementation of IWorker
